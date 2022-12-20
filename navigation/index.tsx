bacbcc7f8369df25cded6c4924bdaa586c742ce5
xA6V5x <ojedaagustinmanuel@gmail.com>
2022-12-20 16:45:53 -0300

    Delete ModalScreen and Performance in Navigation/Index

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,18 +1,11 @@
-/**
- * If you are not familiar with React Navigation, refer to the "Fundamentals" guide:
- * https://reactnavigation.org/docs/getting-started
- *
- */
-import { FontAwesome } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
-import { ColorSchemeName, Pressable } from 'react-native';
+import { ColorSchemeName } from 'react-native';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
-import ModalScreen from '../screens/ModalScreen';
 import TransactionsScreen from '../screens/TransactionsScreen';
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
@@ -33,10 +26,6 @@ export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeNa
      );
 }
 
-/**
- * A root stack navigator is often used for displaying modals on top of all other content.
- * https://reactnavigation.org/docs/modal
- */
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
@@ -47,17 +36,10 @@ function RootNavigator() {
                     component={BottomTabNavigator}
                     options={{ headerShown: false }}
                />
-               <Stack.Group screenOptions={{ presentation: 'modal' }}>
-                    <Stack.Screen name="Modal" component={ModalScreen} />
-               </Stack.Group>
           </Stack.Navigator>
      );
 }
 
-/**
- * A bottom tab navigator displays tab buttons on the bottom of the display to switch screens.
- * https://reactnavigation.org/docs/bottom-tab-navigator
- */
 const BottomTab = createBottomTabNavigator<RootTabParamList>();
 
 function BottomTabNavigator() {
@@ -108,14 +90,4 @@ function BottomTabNavigator() {
                />
           </BottomTab.Navigator>
      );
-}
-
-/**
- * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
- */
-function TabBarIcon(props: {
-     name: React.ComponentProps<typeof FontAwesome>['name'];
-     color: string;
-}) {
-     return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
 }
