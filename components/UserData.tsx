e21db916274fede532922935b394c3e5af510f00
xA6V5x <ojedaagustinmanuel@gmail.com>
2022-12-20 13:29:14 -0300

    Create Component UserData

---
+++ components/UserData.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+import { Text, View } from './Themed';
+
+type userDataProps = {
+     name: string;
+     email: string;
+};
+
+export function UserData({ name, email }: userDataProps) {
+     return (
+          <View key={email} style={styles.container}>
+               <Text style={styles.user_data}> {name} </Text>
+               <Text style={styles.user_data}> {email} </Text>
+          </View>
+     );
+}
+
+const styles = StyleSheet.create({
+     container: {
+          marginTop: 5,
+          marginBottom: 20,
+          width: '100%',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'space-between',
+     },
+     user_data: { fontSize: 20, fontWeight: '500' },
+});
