a3207e0333f6105026778eddf786e262fafe109a
xA6V5x <ojedaagustinmanuel@gmail.com>
2022-12-21 23:24:33 -0300

    Styles AccountName component

--- components/AccountName.tsx
+++ components/AccountName.tsx
@@ -14,10 +14,12 @@ export function AccountName({ accountName }: accountNameProps) {
                                 ...styles.accounts_text,
                                 color: '#3d8ed4',
                            }
-                         : {
+                         : accountName === 'Savings'
+                         ? {
                                 ...styles.accounts_text,
                                 color: '#3dd456',
                            }
+                         : styles.accounts_text
                }
           >
                {accountName}
